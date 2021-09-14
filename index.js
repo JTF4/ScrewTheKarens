c32e5678c1ae56afee071d85ce4b54313cd149a1
David Stevens <teamjtf4@gmail.com>
2021-09-14 10:22:27 -0400

    Performance Improvments

--- index.js
+++ index.js
@@ -5,25 +5,44 @@ const fs = require("fs");
 var app = express();
 const router = express.Router();
 
+var time = new Date();
 
 
-app.listen(4456, () => {
- console.log("Server running on port 4456");
+app.listen(80, () => {
+ console.log("Server running on port 80");
  console.log("Let's screw the Karens");
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-router.post('/',(request,response) => {
+router.post('/*',(request,response) => {
+    console.log(time)
     console.log(request.body);
-    fs.appendFile('list.txt', JSON.stringify(request.body), function (err) {
+    fs.appendFile('list.txt', time, function (err) {
         if (err) throw err;
         console.log(`The ${JSON.stringify(request.body)} was appended to file!`);
     });
+    fs.appendFile('list.txt', JSON.stringify(request.body), function (err) {
+        if (err) throw err;
+    });
     fs.appendFile('list.txt', '\n', function (err) {
         if (err) throw err;
+    });
+});
+
+router.post('/',(request,response) => {
+    console.log(time)
+    console.log(request.body);
+    fs.appendFile('list.txt', time, function (err) {
+        if (err) throw err;
         console.log(`The ${JSON.stringify(request.body)} was appended to file!`);
+    });
+    fs.appendFile('list.txt', JSON.stringify(request.body), function (err) {
+        if (err) throw err;
+    });
+    fs.appendFile('list.txt', '\n', function (err) {
+        if (err) throw err;
     });
 });
 
