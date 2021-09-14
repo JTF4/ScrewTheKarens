d8d743bcfdfcbb90867a27feed6cdb568856e8e0
David Stevens <teamjtf4@gmail.com>
2021-09-13 23:18:27 -0400

    Script

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+var app = express();
+const router = express.Router();
+
+
+
+app.listen(4456, () => {
+ console.log("Server running on port 4456");
+ console.log("Let's screw the Karens");
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+router.post('/',(request,response) => {
+    console.log(request.body);
+    fs.appendFile('list.txt', JSON.stringify(request.body), function (err) {
+        if (err) throw err;
+        console.log(`The ${JSON.stringify(request.body)} was appended to file!`);
+    });
+});
+
+app.use("/", router);
+
+
+
+
